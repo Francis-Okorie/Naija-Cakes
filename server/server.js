45753c41714a65eba8b3db9a6fe488aa56a47fff
Francis Okorie <92164567+Francis-Okorie@users.noreply.github.com>
2025-12-18 08:59:51 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,5 +15,10 @@ app.use(cors({credentials: true}));
 app.get("/", (req,res)=>res.send("API is working"));
 app.use("/api/auth", authRouter)
 app.use("/api/cake", cakeRouter)
+app.get("/api/config", (req, res) => {
+  res.json({
+    apiLink: process.env.PAYPAL_CLIENT_ID
+  });
+});
 
 app.listen(port, ()=> console.log(`Running in port: ${port}`))
